a678c6563c0b8d223dc669bae985950fb9d92fb9
levelfive <levelfive@THINK_X220>
2015-06-30 16:40:35 +0900

    バグを修正（Lの値を階層ごとに保持）

--- src/pgcon06/Pgcon06.java
+++ src/pgcon06/Pgcon06.java
@@ -9,44 +9,45 @@ public class Pgcon06 {
 		Scanner scan = new Scanner(System.in);
 		int c = 0;
 		int L = Integer.parseInt(scan.next());
-		int Ltemp = L;
+		int L2, L3;
 		int N = Integer.parseInt(scan.next());
 		List<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < N; i++) {
-
 			list.add(Integer.parseInt(scan.next()));
 		}
+
 		// 1つ目の長さ
 		for (int j = 0; j < N; j++) {
 			// 棒の長さを入力値が超えたら弾く
 			if ((L - list.get(j)) <= 0) {
 				continue;
 			}
-			L = (L - list.get(j));
-			// int firstnum = list.get(j);
+			L2 = (L - list.get(j));
+			int firstnum = list.get(j);
+
 			// 2つ目の数字
 			for (int k = 0; k < N; k++) {
-				// 一番目と同じ要素を弾く
+				// // 一番目と同じ要素を弾く
 				if (k == j) {
 					continue;
 				}
 
-				if ((L - list.get(k)) <= 0) {
+				if ((L2 - list.get(k)) <= 0) {
 					continue;
 				}
-				L = (L - list.get(k));
-				// int secondnum = list.get(k);
+				L3 = (L2 - list.get(k));
+				int secondnum = list.get(k);
 				// 3つ目の長さ
 				for (int l = 0; l < N; l++) {
+
 					// 1,2番目と同じ要素を弾く。
 					if (l == k || l == j) {
 						continue;
 					}
-					if ((L - list.get(l)) == 0) {
-						L = (L - list.get(l));
-						// int thirdnum = list.get(l);
+					if ((L3 - list.get(l)) == 0) {
+						int thirdnum = list.get(l);
 						c++;
-						L = Ltemp;
+
 						break;
 					} else {
 						continue;
@@ -54,7 +55,7 @@ public class Pgcon06 {
 				}// 3つ目の長さ
 			}// ２つ目の長さ
 		}// １つ目の長さ
-		System.out.println("組み合わせは" + c + "通りです");
+		System.out.println("組み合わせは" + (c / 6) + "通りです");
 	}
 
 }
