7590358fb58239ff3e2a909ac45539d99d0c20fe
健悟 <@sonntyou>
2015-07-09 00:40:45 +0900

    クイックソートを追加 重複計算を除去

--- src/pgcon06/Pgcon06.java
+++ src/pgcon06/Pgcon06.java
@@ -1,51 +1,45 @@
 package pgcon06;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Pgcon06 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int c = 0;
-		int L = Integer.parseInt(scan.next());
+		System.out.println("棒の長さを入力してください。");
+		int L = scan.nextInt();
 		int L2, L3;
-		int N = Integer.parseInt(scan.next());
-		List<Integer> list = new ArrayList<Integer>();
+		System.out.println("要素数を入力してください。");
+		int N = (scan.nextInt());
+		// List<Integer> list = new ArrayList<Integer>();
+		int list[] = new int[N];
 		for (int i = 0; i < N; i++) {
-			list.add(Integer.parseInt(scan.next()));
+			list[i] = scan.nextInt();
 		}
+
+		quickSort(list, 0, (N - 1));
 
 		// 1つ目の長さ
 		for (int j = 0; j < N; j++) {
-			// 棒の長さを入力値が超えたら弾く
-			if ((L - list.get(j)) <= 0) {
-				continue;
+			// 一つ目の棒の長さが入力値を超えたら終了
+			// または、入力値の半分を超えたら終了
+			if ((L - list[j]) <= 0 || list[j] > (L / 2)) {
+				break;
 			}
-			L2 = (L - list.get(j));
-			int firstnum = list.get(j);
+			L2 = (L - list[j]);
 
 			// 2つ目の数字
-			for (int k = 0; k < N; k++) {
-				// // 一番目と同じ要素を弾く
-				if (k == j) {
+			for (int k = j + 1; k < N; k++) {
+				// 1,2番目の合計値が棒の長さを超えたら終了
+				// または2番目の長さの次の要素が残りの数より小さかったら終了。
+				if ((L2 - list[k]) <= 0 || (L2 - list[k]) > list[k + 1]) {
 					continue;
 				}
-
-				if ((L2 - list.get(k)) <= 0) {
-					continue;
-				}
-				L3 = (L2 - list.get(k));
-				int secondnum = list.get(k);
+				L3 = (L2 - list[k]);
 				// 3つ目の長さ
-				for (int l = 0; l < N; l++) {
-
-					// 1,2番目と同じ要素を弾く。
-					if (l == k || l == j) {
-						continue;
-					}
-					if ((L3 - list.get(l)) == 0) {
-						int thirdnum = list.get(l);
+				for (int l = k + 1; l < N; l++) {
+					//
+					if ((L3 - list[l]) == 0) {
 						c++;
 
 						break;
@@ -55,7 +49,59 @@ public class Pgcon06 {
 				}// 3つ目の長さ
 			}// ２つ目の長さ
 		}// １つ目の長さ
-		System.out.println("組み合わせは" + (c / 6) + "通りです");
+		System.out.println("組み合わせは" + (c) + "通りです");
+	}
+
+	// 以下クイックソート用
+	public static int base(int[] a, int first, int end) {
+		int second = first + 1;
+		while (second <= end && a[first] == a[second]) {
+			second++;
+		}
+		if (second > end) {
+			return -1;
+		}
+		if (a[first] >= a[second]) {
+			return first;
+		} else {
+			return second;
+		}
+
+	}
+
+	public static int partition(int[] a, int i, int j, int x) {
+		int l = i, r = j;
+
+		while (l <= r) {
+
+			while (l <= j && a[l] < x) {
+				l++;
+			}
+
+			while (r >= i && a[r] >= x) {
+				r--;
+			}
+
+			if (l > r)
+				break;
+			int t = a[l];
+			a[l] = a[r];
+			a[r] = t;
+			l++;
+			r--;
+		}
+		return l;
+	}
+
+	public static void quickSort(int[] a, int i, int j) {
+		if (i == j)
+			return;
+		int p = base(a, i, j);
+		if (p != -1) {
+			int k = partition(a, i, j, a[p]);
+			quickSort(a, i, k - 1);
+			quickSort(a, k, j);
+		}
 	}
 
 }
